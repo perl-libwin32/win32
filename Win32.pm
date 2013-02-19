26f32fc57be2fea8cfd9b4c340fc14e645e8f1f9
Jan Dubois <jand@activestate.com>
2013-02-19 12:43:19 -0800

    Update to version 0.46

--- Win32.pm
+++ Win32.pm
@@ -8,7 +8,7 @@ package Win32;
     require DynaLoader;
 
     @ISA = qw|Exporter DynaLoader|;
-    $VERSION = '0.45';
+    $VERSION = '0.46';
     $XS_VERSION = $VERSION;
     $VERSION = eval $VERSION;
 
