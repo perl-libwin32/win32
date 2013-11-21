4aef793ac18820be18635e84d8f8b866095b66aa
Jan Dubois <jand@activestate.com>
2013-11-20 18:03:42 -0800

    Prepare for 0.48 release

--- Win32.pm
+++ Win32.pm
@@ -8,7 +8,7 @@ package Win32;
     require DynaLoader;
 
     @ISA = qw|Exporter DynaLoader|;
-    $VERSION = '0.47';
+    $VERSION = '0.48';
     $XS_VERSION = $VERSION;
     $VERSION = eval $VERSION;
 
