2e22828fbc3cec2b37dc7e4e218629e572647e61
Jan Dubois <jan@jandubois.com>
2020-03-27 22:38:25 -0700

    Prepare for Win32-0.54 release

--- Win32.pm
+++ Win32.pm
@@ -8,7 +8,7 @@ package Win32;
     require DynaLoader;
 
     @ISA = qw|Exporter DynaLoader|;
-    $VERSION = '0.53';
+    $VERSION = '0.54';
     $XS_VERSION = $VERSION;
     $VERSION = eval $VERSION;
 
