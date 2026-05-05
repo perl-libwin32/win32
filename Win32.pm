90b321b82a0a16a71a53d22891e39ee6db207557
Jan Dubois <jan@jandubois.com>
2026-05-04 21:35:11 -0700

    Prepare for v0.60 release

--- Win32.pm
+++ Win32.pm
@@ -8,7 +8,7 @@ package Win32;
     require DynaLoader;
 
     @ISA = qw|Exporter DynaLoader|;
-    $VERSION = '0.59_02';
+    $VERSION = '0.60';
     $XS_VERSION = $VERSION;
     $VERSION = eval $VERSION;
 
